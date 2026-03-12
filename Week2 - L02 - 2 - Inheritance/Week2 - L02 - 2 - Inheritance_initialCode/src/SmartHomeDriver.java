bc128eaf8820b718f0479b4ff94b7ee4af004216
itsdragzz <kelvinliang123@icloud.com>
2026-03-12 12:34:53 +1100

    Add initial implementation of SmartHome devices and update .gitignore

---
+++ Week2 - L02 - 2 - Inheritance/Week2 - L02 - 2 - Inheritance_initialCode/src/SmartHomeDriver.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+
+public class SmartHomeDriver {
+
+    public static void main(String[] args) {
+
+        //SETUP IOT Ecosystem
+        SmartSwitch smartSwitch1=new SmartSwitch("128.1.1.1",false);
+        smartSwitch1.addBulb(new SmartBulb("128.1.1.2",false,"red","dimmable"));
+        smartSwitch1.addBulb(new SmartBulb("128.1.1.3",false,"blue", "nonDimmable"));
+
+        SmartTV smartTVV1 = new SmartTV("128.1.1.4", false);
+
+        SmartHeater smartHeater1= new SmartHeater("128.1.1.5", false);
+        SmartHeater smartHeater2= new SmartHeater("128.1.1.6", false);
+
+        LedStripe ledStripe1 = new LedStripe("128.1.1.7",false,"yellow", "dimmable");
+        LedStripe ledStripe2 = new LedStripe("128.1.1.8",false,"yellow", "dimmable");
+        ledStripe1.attachLedStripe(ledStripe2);
+
+
+
+        //LOAD CONTROL MENU
+        int selection;
+        do {
+            selection = menuItem();
+            switch (selection) {
+                case 1:
+                    smartSwitch1.turnOn();
+                    smartSwitch1.turnAllOn("red");
+                    smartTVV1.turnOn();
+                    smartHeater1.turnOn(20);
+                    smartHeater2.turnOn(20);
+                    ledStripe1.turnOn();
+                    break;
+                case 2:
+                    smartSwitch1.turnOff();
+                    smartSwitch1.turnAllOff();
+                    smartTVV1.turnOff();
+                    smartHeater1.turnOff();
+                    smartHeater2.turnOff();
+                    ledStripe1.turnOff();
+                    break;
+                case 3:
+                    smartSwitch1.display();
+                    smartTVV1.display();
+                    smartHeater1.display();
+                    smartHeater2.display();
+                    ledStripe1.display();
+                    break;
+
+                case 0:
+                    System.exit(0);
+            }
+
+
+        } while (selection != 7);
+    }
+
+    public static int menuItem() {
+            Scanner sel = new Scanner(System.in);
+
+            System.out.println("+-------------------------------------+");
+            System.out.println("|            Welcome to the           |");
+            System.out.println("|      Smart Home control system      |");
+            System.out.println("+-------------------------------------+");
+
+            System.out.println("1) Turn all devices ON");
+            System.out.println("2) Turn all devices OFF");
+            System.out.println("3) Display status of all devices");
+
+
+            System.out.println("0) Exit");
+            System.out.print("Select an option:");
+            int choice = Integer.parseInt(sel.nextLine()); // try catch must be added
+            return choice;
+        }
+
+
+
+}
---
+++ Week2 - L02 - 2 - Inheritance/Week2 - L02 - 2 - Inheritance_initialCode/src/SmartSwitch.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+
+public class SmartSwitch {
+
+    //private attributes
+    private String ipAddress;
+    private boolean status;
+    private double consumptionToday; //inWatts
+    private ArrayList<SmartBulb> smartBulbs;
+
+    //constructor
+    public SmartSwitch(String _ipAddress, boolean _status) {
+        this.ipAddress = _ipAddress;
+        this.status = _status;
+        this.smartBulbs = new ArrayList<SmartBulb>();
+    }
+
+
+
+    //getters and setters
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public double getConsumptionToday() {
+        return Utils.getRandomConsumption();
+    }
+
+    //Other methods
+    public void turnOn() {
+        this.status = true;
+        System.out.println("The SmartSwitch at "+ this.ipAddress +" was turned ON successfully");
+    }
+
+    public void turnOff() {
+        this.status = false;
+        System.out.println("The SmartSwitch at "+ this.ipAddress +" was turned OFF successfully");
+    }
+
+
+    public void addBulb(SmartBulb _smartBulb) {
+        this.smartBulbs.add(_smartBulb);
+    }
+
+    public ArrayList<SmartBulb> getBulbs(){
+        return this.smartBulbs;
+    }
+
+    public void turnAllOn(String colour) {
+        this.status = true;
+        for (SmartBulb smartbulb : smartBulbs
+        ) {
+            smartbulb.turnOn();
+        }
+    }
+
+    public void turnAllOff() {
+        this.status = false;
+        for (SmartBulb smartbulb : smartBulbs
+        ) {
+            smartbulb.turnOff();
+        }
+    }
+
+    public double getTotalConsumptionToday() {
+        double totalConsumption = 0;
+        totalConsumption += this.consumptionToday;
+        for (SmartBulb smartbulb : smartBulbs
+        ) {
+            totalConsumption += smartbulb.getConsumptionToday();
+        }
+        return totalConsumption;
+    }
+
+    public void display() {
+        System.out.println("SmartSwitch  at " + this.getIpAddress() + " is on: " + this.getStatus() + " Today's consumption: " + this.getConsumptionToday());
+        int i = 0;
+        for (SmartBulb smartBulb : this.getBulbs()) {
+            i++;
+            System.out.println("Bulb " + i + " at " + smartBulb.getIpAddress() + " is on: " + smartBulb.getStatus() + ". Colour:" + smartBulb.getColour() + ". Today's consumption: " + smartBulb.getConsumptionToday());
+        }
+    }
+
+
+}
